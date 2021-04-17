518a33dc139cb22d26fde7fc3d341d3c91e4f73e
Manan Doshi <manandoshi1301@gmail.com>
2021-04-17 19:46:38 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import LogSign from "./components/LogSign/LogSign";
+function App() {
+  return (
+    <div className="App">
+      <LogSign></LogSign>
+    </div>
+  );
+}
+
+export default App;
