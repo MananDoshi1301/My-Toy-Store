fc3c0c0bb6f3ced82b64883be0fb023f6429d9b0
Manan Doshi <manandoshi1301@gmail.com>
2021-04-18 00:06:38 +0530

    update commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import LogSign from "./components/LogSign/LogSign";
+import Dashboard from "./components/Dashboard/Dashboard";
 function App() {
   return (
     <div className="App">
-      <LogSign></LogSign>
+      {/* <LogSign /> */}
+      <Dashboard />
     </div>
   );
 }
---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Nav from '../Nav'
+
+const Dashboard = () => {
+    return (
+        <>
+            <Nav></Nav>
+        </>
+    )
+}
+
+export default Dashboard
