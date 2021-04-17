194d614776d074bbb03c38903e6d989aa35f103e
Manan Doshi <manandoshi1301@gmail.com>
2021-04-18 02:51:15 +0530

    indent update

--- src/components/LogSign/LogSign.js
+++ src/components/LogSign/LogSign.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import Login from './LogIn/Login'
 import Signup from './Signup/Signup'
 
@@ -8,14 +8,14 @@ const LogSign = () => {
     let [login, setLogin] = useState(true);
 
     return (
-        <div className = {"overflow-hidden"}>
+        <div className={"overflow-hidden"}>
             <nav className="navbar navbar-dark bg-danger">
                 <div className="container-fluid">
                     <a className="navbar-brand fw-bold fs-1">MyToyStore</a>
-                    <button className={"btn btn-warning btn-lg me-2"} type="button" onClick={()=>{setLogin(!login)}}>{login? `SignIn`:`LogIn`}</button>
+                    <button className={"btn btn-warning btn-lg me-2"} type="button" onClick={() => { setLogin(!login) }}>{login ? `SignIn` : `LogIn`}</button>
                 </div>
             </nav>
-            {login?<Login />:<Signup />}
+            {login ? <Login /> : <Signup />}
         </div>
     )
 }
