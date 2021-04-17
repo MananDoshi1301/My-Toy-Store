9bb2798974d3fa2e1178dc8af29eb1b50b073682
Manan Doshi <manandoshi1301@gmail.com>
2021-04-18 04:46:58 +0530

    dashboard classes updated

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import Nav from '../Nav'
+import AgeCategory from './AgeCategory/AgeCategory'
 import Carousel from './Carousel/Carousel'
 
 const Dashboard = () => {
+
+    const colors = ['danger', 'info', 'primary', 'warning', 'success', 'secondary']
+    const category = {
+        age:['8', '13', '3 - 4', '5 - 6', '7 - 9', '10 - 12']
+    };
+
     return (
         <>
             <Nav></Nav>
             <Carousel></Carousel>
+            <div className="container-fluid">
+                <AgeCategory age={category.age} colors={colors}></AgeCategory>
+
+            </div>
         </>
     )
 }
