a7fba7f98ef34c1e16ff903235c15d88224736ca
Manan Doshi <manandoshi1301@gmail.com>
2021-04-17 20:42:31 +0530

    nav button updated

--- src/components/LogSign/LogSign.js
+++ src/components/LogSign/LogSign.js
@@ -12,7 +12,7 @@ const LogSign = () => {
             <nav className="navbar navbar-dark bg-danger">
                 <div className="container-fluid">
                     <a className="navbar-brand fw-bold fs-1">MyToyStore</a>
-                    <button className={"btn btn-warning btn-lg"} onClick={()=>{setLogin(!login)}}>{login? `SignIn`:`LogIn`}</button>
+                    <button className={"btn btn-warning btn-lg me-2"} type="button" onClick={()=>{setLogin(!login)}}>{login? `SignIn`:`LogIn`}</button>
                 </div>
             </nav>
             {login?<Login />:<Signup />}
