3c0c252985ae519e7e75feca678087c21060df42
Manan Doshi <manandoshi1301@gmail.com>
2021-04-18 04:59:36 +0530

    Dashboard updated with props category value for nav droplink

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -6,17 +6,19 @@ import Carousel from './Carousel/Carousel'
 const Dashboard = () => {
 
     const colors = ['danger', 'info', 'primary', 'warning', 'success', 'secondary']
+
+    const navDropDownCategories = ['Vehicle And Remote Control', 'Dolls', 'Action-Figures','Bikes and Riding Toys','Games And Puzzles','Soft Toys'];
+
     const category = {
         age:['8', '13', '3 - 4', '5 - 6', '7 - 9', '10 - 12']
     };
 
     return (
         <>
-            <Nav></Nav>
+            <Nav categories={navDropDownCategories}></Nav>
             <Carousel></Carousel>
             <div className="container-fluid">
                 <AgeCategory age={category.age} colors={colors}></AgeCategory>
-
             </div>
         </>
     )
