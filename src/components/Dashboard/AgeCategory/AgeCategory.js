94b8fff629aa0ef5f1f07c1a1206c937a3d7d4a1
Manan Doshi <manandoshi1301@gmail.com>
2021-04-18 15:35:14 +0530

    Text centered!

--- src/components/Dashboard/AgeCategory/AgeCategory.js
+++ src/components/Dashboard/AgeCategory/AgeCategory.js
@@ -2,11 +2,12 @@ import React from 'react'
 
 const AgeCategory = ({age, colors}) => {
     const btns = age.map(age=>
-        <button className={`btn btn-lg btn-${colors[Math.floor(Math.random()*colors.length)]} rounded-pill fs-3`}>{age.length<=2?`${age} years and above`:`${age} years`}</button>
+        <button className={`btn btn-lg btn-${colors[Math.floor(Math.random()*colors.length)]} rounded-pill border border-5 fs-3`}>{age.length<=2?`${age} years and above`:`${age} years`}</button>
     );
+    
     return (
         <>
-        <div className={`display-4 my-4 mt-5`}>
+        <div className={`display-1 my-4 mt-5 text-center`}>
           Shop By Age  
         </div>
         <div className={`d-flex justify-content-around`}>  
