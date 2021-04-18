8d9fb78acbdb347d53299acd7fe1d0c4c77de8d8
Manan Doshi <manandoshi1301@gmail.com>
2021-04-18 15:33:44 +0530

    Added Shadow to categories and head centered

--- src/components/Dashboard/TypeCategory/TypeCategory.js
+++ src/components/Dashboard/TypeCategory/TypeCategory.js
@@ -4,9 +4,9 @@ const TypeCategory = (props) => {
 
     const cardImageDisplay = props.typeObj.map(obj=>{
         return <div class="col">
-        <div class="card">
+        <div class="card shadow p-0 mb-5 bg-body rounded">
             <img src={obj.img} class="card-img-top" alt={obj.Name}></img>
-            <div class="card-body text-center">
+            <div class="card-body text-center ">
                 <p class="card-title fs-2">{obj.Name}</p>
             </div>
         </div>
@@ -15,7 +15,7 @@ const TypeCategory = (props) => {
 
     return (
         <>  
-            <div className={`display-4 my-4 mt-5`}>
+            <div className={`display-1 my-4 mt-5 text-center`}>
                 Shop By Category
             </div>
             <div class="row row-cols-1 row-cols-md-3 g-4">
