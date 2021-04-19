ddad094921600ff04e5b1679a0e735983540c6a9
MananDoshi1301 <manandoshi1301@gmail.com>
2021-04-19 23:22:25 +0530

    prodDetails changehandleevent added

--- src/components/LogSign/Admin/Admin.js
+++ src/components/LogSign/Admin/Admin.js
@@ -1,17 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 
 import {category} from "../../Data/Data";
 
 const Admin = () => {
 
+  const productDetail = {
+    prodImage:null,
+    prodName:null,
+    prodType:null,
+    prodBrand:null,
+    prodColor:null,
+  }
+
+  let [stateDetails, setStateDetails] = useState(productDetail);
+
+  const handleNewProduct = (e) => {
+    const {name, value} = e.target;
+    setStateDetails({
+      ...stateDetails,
+      [name]:value
+    });
+    console.log(stateDetails);
+  };
+
+
   const typeCategory = category.typeImgs.map((obj)=>{
     return <option value={obj.Name}>{obj.Name}</option>
   })
-
   const brandCategory = category.brandImgs.map( obj=>{
     return <option value={obj.Name}>{obj.Name}</option>
   })
-
   const colorCategory = category.color.map( obj=>{
     return <option value={obj.Name}>{obj.Name}</option>
   })
@@ -23,50 +41,62 @@ const Admin = () => {
 
         <div className="row mt-3">
           <div className="col-lg-5">
-            <label className={`form-label fs-3 mt-3`} htmlFor="productImage">
-              Product Image:
-            </label>
-            <input
-              className={`form-control`}
-              type="file"
-              id="productImage"
-              name="prodImage"
-              accept="image/png, image/jpeg, image/jpg"
-            />
 
-            <label className={`form-label fs-3 mt-3`} htmlFor="productName">
-              Product Name
-            </label>
-            <input
-              className={`form-control`}
-              type="text"
-              id="productName"
-              name="prodName"
-            />
+            <form>
+              <label className={`form-label fs-3 mt-3`} htmlFor="productImage">
+                Product Image:
+              </label>
+              <input
+                className={`form-control`}
+                type="file"
+                id="productImage"
+                name="prodImage"
+                accept="image/png, image/jpeg, image/jpg"
+                onChange={handleNewProduct} required
+              />
 
-            <label className={`form-label fs-3 mt-3`} htmlFor="productType">
-              Product Type
-            </label>
-            <select className={`form-control`} name="" id="productType">
-              {typeCategory}
-            </select>
+              <label className={`form-label fs-3 mt-3`} htmlFor="productName">
+                Product Name
+              </label>
+              <input
+                className={`form-control`}
+                type="text"
+                id="productName"
+                name="prodName"
+                onChange={handleNewProduct} required
+              />
 
-
-            <label className={`form-label fs-3 mt-3`} htmlFor="productBrand">
-              Product Brand
-            </label>
-            <select className={`form-control`} name="" id="productBrand">
-              {brandCategory}
-            </select>
-
-            <label className={`form-label fs-3 mt-3`} htmlFor="productColor">
-              Product Color
-            </label>
-            <select className={`form-control`} name="" id="productColor">
-              {colorCategory}
-            </select>
+              <label className={`form-label fs-3 mt-3`} htmlFor="productType">
+                Product Type
+              </label>
+              <select className={`form-control`} name="" id="productType"
+                onChange={handleNewProduct} required
+              >
+                {typeCategory}
+              </select>
 
 
+              <label className={`form-label fs-3 mt-3`} htmlFor="productBrand">
+                Product Brand
+              </label>
+              <select className={`form-control`} name="" id="productBrand"
+                onChange={handleNewProduct} required
+                value={value}
+              >
+                {brandCategory}
+              </select>
+
+              <label className={`form-label fs-3 mt-3`} htmlFor="productColor">
+                Product Color
+              </label>
+              <select className={`form-control`} name="" id="productColor"
+                onChange={handleNewProduct} required
+              >
+                {colorCategory}
+              </select>
+
+              <button type="submit" className="mt-3 btn btn-primary" >Submit</button>
+            </form>
           </div>
         </div>
       </div>
