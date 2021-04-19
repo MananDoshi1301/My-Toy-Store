a7e1c04ddb99f6d063c6e0b451fb877199c00dff
MananDoshi1301 <manandoshi1301@gmail.com>
2021-04-19 22:32:26 +0530

    Data handling added!

--- src/components/LogSign/LogSign.js
+++ src/components/LogSign/LogSign.js
@@ -1,12 +1,41 @@
 import React, { useState } from 'react'
+import Admin from './Admin/Admin';
 import Login from './LogIn/Login'
 import Signup from './Signup/Signup'
 
 
 const LogSign = () => {
 
-    let [login, setLogin] = useState(true);
+    let [login, setLogin] = useState(true);    
 
+    const initialDetails = {
+        fullName:null,
+        password:null,
+        email:null,
+        contact:null
+    };
+
+    let [details,setDetails] = useState(initialDetails);
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setDetails({
+          ...details,
+          [name]: value,
+        });        
+    };    
+
+    const submitData = () => {
+        
+        if(login === false){
+
+        }
+        else{
+
+        }
+        console.log(details);
+    }
+    
     return (
         <div className={"overflow-hidden"}>
             <nav className="navbar navbar-dark bg-danger">
@@ -15,7 +44,10 @@ const LogSign = () => {
                     <button className={"btn btn-warning btn-lg me-2"} type="button" onClick={() => { setLogin(!login) }}>{login ? `SignIn` : `LogIn`}</button>
                 </div>
             </nav>
-            {login ? <Login /> : <Signup />}
+            {/* {login ? 
+            <Login changeDetail={handleInputChange} onSubmit={submitData} /> : 
+            <Signup changeDetail={handleInputChange} onSubmit={submitData} />} */}
+            <Admin></Admin>
         </div>
     )
 }
