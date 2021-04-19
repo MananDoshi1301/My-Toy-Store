467d2b71c34afe89cd690721c00db51a2b7246cc
MananDoshi1301 <manandoshi1301@gmail.com>
2021-04-19 20:10:34 +0530

    Firebase configuration created

---
+++ src/Firebase/config.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase/app';
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDzJ6DrHvoM7XmtlX2W5iD9X162uMySwOA",
+  authDomain: "my-toy-store-4e5e8.firebaseapp.com",
+  projectId: "my-toy-store-4e5e8",
+  storageBucket: "my-toy-store-4e5e8.appspot.com",
+  messagingSenderId: "905129563250",
+  appId: "1:905129563250:web:51bb90f345749a0d72deeb"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
