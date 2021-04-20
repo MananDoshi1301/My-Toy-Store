15e4aa27f24625cbcc8eab260de2e40bbac66bf8
Manan Doshi <manandoshi1301@gmail.com>
2021-04-21 02:01:52 +0530

    Updated with form tags for submit of data

--- src/components/LogSign/Admin/Admin.js
+++ src/components/LogSign/Admin/Admin.js
@@ -44,80 +44,81 @@ const Admin = () => {
           ></UploadProgress>}
 
         <div className="row mt-3">
-          <div className="col-lg-5">            
-            {/* Product Image */}
-            <label className={`form-label fs-3 mt-3`} htmlFor="productImage">
-              Product Image:
+          <div className="col-lg-5">   
+            <form>
+              {/* Product Image */}
+              <label className={`form-label fs-3 mt-3`} htmlFor="productImage">
+                Product Image:
             </label>
-            <input
-              className={`form-control`}
-              type="file"
-              id="productImage"
-              name="prodImage"
-              accept="image/png, image/jpeg, image/jpg"
-              onChange={handleNewProduct} required
-            />
+              <input
+                className={`form-control`}
+                type="file"
+                id="productImage"
+                name="prodImage"
+                accept="image/png, image/jpeg, image/jpg"
+                onChange={handleNewProduct} required
+              />
 
-            {/* Product Name */}
-            <label className={`form-label fs-3 mt-3`} htmlFor="productName">
-              Product Name
+              {/* Product Name */}
+              <label className={`form-label fs-3 mt-3`} htmlFor="productName">
+                Product Name
             </label>
-            <input
-              className={`form-control`}
-              type="text"
-              id="productName"
-              name="prodName"
-              onChange={handleNewProduct} required
-            />
+              <input
+                className={`form-control`}
+                type="text"
+                id="productName"
+                name="prodName"
+                onChange={handleNewProduct} required
+              />
 
-            {/* Product Type */}
-            <label className={`form-label fs-3 mt-3`} htmlFor="productType">
-              Product Type
+              {/* Product Type */}
+              <label className={`form-label fs-3 mt-3`} htmlFor="productType">
+                Product Type
             </label>
-            <select className={`form-control`}
-              id="productType"
-              onChange={handleNewProduct}
-              name="prodType" //imp
-              required
-            >
-              {category.typeImgs.map((obj) => {
-                return <option value={obj.value}>{obj.Name}</option>
-              })}
-            </select>
+              <select className={`form-control`}
+                id="productType"
+                onChange={handleNewProduct}
+                name="prodType" //imp
+                required
+              >
+                {category.typeImgs.map((obj) => {
+                  return <option value={obj.value}>{obj.Name}</option>
+                })}
+              </select>
 
-            {/* Product Brand */}
-            <label className={`form-label fs-3 mt-3`} htmlFor="productBrand">
-              Product Brand
+              {/* Product Brand */}
+              <label className={`form-label fs-3 mt-3`} htmlFor="productBrand">
+                Product Brand
             </label>
-            <select className={`form-control`} name="" id="productBrand"
-              onChange={handleNewProduct}
-              name="prodBrand"
-              required
-            >
-              {category.brandImgs.map(obj => {
-                return <option value={obj.value}>{obj.value}</option>
-              })}
-            </select>
+              <select className={`form-control`} name="" id="productBrand"
+                onChange={handleNewProduct}
+                name="prodBrand"
+                required
+              >
+                {category.brandImgs.map(obj => {
+                  return <option value={obj.value}>{obj.value}</option>
+                })}
+              </select>
 
-            {/* Product Color */}
-            <label className={`form-label fs-3 mt-3`} htmlFor="productColor">
-              Product Color
+              {/* Product Color */}
+              <label className={`form-label fs-3 mt-3`} htmlFor="productColor">
+                Product Color
             </label>
-            <select className={`form-control`}
-              name="prodColor"
-              id="productColor"
-              onChange={handleNewProduct} required
-            >
-              {category.color.map(obj => {
-                return <option value={obj.value}>{obj.value}</option>
-              })}
-            </select>
+              <select className={`form-control`}
+                name="prodColor"
+                id="productColor"
+                onChange={handleNewProduct} required
+              >
+                {category.color.map(obj => {
+                  return <option value={obj.value}>{obj.value}</option>
+                })}
+              </select>
 
-            <button
-              // type="submit"
-              className="mt-3 btn btn-primary"
-              onClick={() => { sendData(stateDetails) }}
-            >Submit</button>
+              <button
+                className="mt-3 btn btn-primary"
+                onClick={() => { sendData(stateDetails) }}
+              >Submit</button>
+            </form>
           </div>
         </div>
       </div>
