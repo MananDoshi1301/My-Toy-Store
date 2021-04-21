4f88187873c9078b4325fe5d2cac2af62986f44b
MananDoshi1301 <manandoshi1301@gmail.com>
2021-04-22 01:03:14 +0530

    Added Link to TypeCategoory dashboard items

--- src/components/Dashboard/TypeCategory/TypeCategory.js
+++ src/components/Dashboard/TypeCategory/TypeCategory.js
@@ -1,22 +1,24 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom';
 import styles from "../../component.module.css"
 
 const TypeCategory = (props) => {
 
-    const cardImageDisplay = props.typeObj.map(obj=>{
+    const cardImageDisplay = props.typeObj.map(obj => {
         return <div class="col">
-        <div class="card shadow p-0 mb-5 bg-body rounded">
-            <img src={obj.img} class="card-img-top" alt={obj.Name}></img>
-            <div class="card-body text-center ">
-                <p class={`card-title fs-3 ${styles.bangers}`}>{obj.Name}</p>
+            <div class="card shadow p-0 mb-5 bg-body rounded">
+                <Link to={`/product/${obj.Name}`}>
+                    <img src={obj.img} class="card-img-top" alt={obj.Name}></img>
+                    <div class="card-body text-center ">
+                        <p class={`card-title fs-3 ${styles.bangers}`}>{obj.Name}</p>
+                    </div>
+                </Link>
             </div>
         </div>
-    </div>
     })
 
     return (
-        <>  
+        <>
             <div className={`display-1 my-4 mt-5 text-center fw-bold ${styles.architectDaughters} bg-success`}>
                 Shop By Category
             </div>
