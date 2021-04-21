2090cc420f3c34e55f136973ee869f8f52ed477b
MananDoshi1301 <manandoshi1301@gmail.com>
2021-04-21 17:44:02 +0530

    Redirecting in signup successful

--- src/components/LogSign/Signup/NewUserProgress.js
+++ src/components/LogSign/Signup/NewUserProgress.js
@@ -1,17 +1,19 @@
-import React,{useEffect} from 'react';
+import React, { useEffect } from 'react';
+import { useHistory } from "react-router-dom";
 
-import { projectStorage,projectFirestore, timestamp } from "../../../Firebase/config";
+import { projectStorage, projectFirestore, timestamp } from "../../../Firebase/config";
 
-const NewUserProgress = ({file, setFile}) => {
-    useEffect(()=>{
+const NewUserProgress = ({ file, setFile }) => {
+    const history = useHistory();
+    useEffect(() => {
         console.log(file);
         const userRef = projectFirestore.collection('userDetails');
-        userRef.add({...file}).then(alert("User Registered!"));
+        userRef.add({ ...file }).then(alert("User Registered!")).then(history.push('/'));
         // (setFile(false))
-    },[file])
+    }, [file])
     return (
         <div>
-            {}
+            { }
         </div>
     )
 }
