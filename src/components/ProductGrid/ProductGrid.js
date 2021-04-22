1e9d315a26ae77670d90fad8a20fa7eac9832c64
MananDoshi1301 <manandoshi1301@gmail.com>
2021-04-23 04:30:34 +0530

    Card limited from 4 to 3 in a row

--- src/components/ProductGrid/ProductGrid.js
+++ src/components/ProductGrid/ProductGrid.js
@@ -34,7 +34,7 @@ const ProductGrid = ({ cartItems, setCartItems }) => {
         setCartItems={setCartItems}
       />
       <div className="container my-5">
-        <div class="row row-cols-1 row-cols-md-4 g-4">
+        <div class="row row-cols-1 row-cols-md-3 g-4">
           {docs &&
             docs
               .filter((doc) => {
