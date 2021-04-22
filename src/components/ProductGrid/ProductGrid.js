bff35e031e3b2d69d4da3e1ba62f16535beb4007
MananDoshi1301 <manandoshi1301@gmail.com>
2021-04-22 12:26:09 +0530

    Product list display successful

--- src/components/ProductGrid/ProductGrid.js
+++ src/components/ProductGrid/ProductGrid.js
@@ -3,31 +3,42 @@ import { useParams } from "react-router-dom";
 import Nav from "../Nav";
 import FetchData from "../FetchData";
 import { category } from "../Data/Data";
-import ProductCard from "./ProductCard/ProductCard";
 
 const ProductGrid = () => {
   const { docs } = FetchData("products"); //Fetch from db
   let { categoryType, itemType } = useParams(); //Fetch params from links
-  // console.log(categoryType, itemType, docs);
-  const [products, setProducts] = useState([]);
-  useEffect(() => {
-    let tempProducts = [];
-    for (let i = 0; i < docs.length; i++) {
-      if (docs[i].file[categoryType] === itemType) {
-        let obj = docs[i].file;
-        tempProducts.push(obj);
-        console.log(tempProducts);
-      }
-    }
-    console.log(tempProducts);
-    setProducts(tempProducts);
-  }, [itemType]);
+  console.log(categoryType, itemType, docs);
 
   return (
     <>
       <Nav categories={category.typeImgs} />
-      <div className={`container-fluid my-5`}>
-        <ProductCard products={products} />
+      <div className="container my-5">
+        <div class="row row-cols-1 row-cols-md-4 g-4">
+          {docs &&
+            docs
+              .filter((doc) => {
+                return doc.file[categoryType] === itemType;
+              })
+              .map((product) => {
+                return (
+                  <div class="col">
+                    <div class="card h-100">
+                      <img
+                        src={product.url}
+                        class="card-img-top img-fluid"
+                        alt={product.file.prodName}
+                      />
+                      <div class="card-body">
+                        <h5 class="card-title">{product.file.prodName}</h5>
+                        <a href="#" class="btn btn-primary">
+                          Add To Cart
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+        </div>
       </div>
     </>
   );
