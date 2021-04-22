73173f419402094885ff387979f451db509d2d87
MananDoshi1301 <manandoshi1301@gmail.com>
2021-04-22 16:21:32 +0530

    Prood passing created

---
+++ src/components/ProductGrid/AddProductDB.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { projectStorage, projectFirestore, timestamp } from "../../Firebase/config";
+
+const AddProductDB = ({ id, resetAdded }) => {
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default AddProductDB
