562582abbe5bb89aceaa9adf25eb287628306331
Manan Doshi <manandoshi1301@gmail.com>
2021-05-05 21:59:34 +0530

    Modified getOrder

--- src/components/GetOrders.js
+++ src/components/GetOrders.js
@@ -1,13 +1,43 @@
 import React from 'react'
 import { useParams } from "react-router-dom";
 import FetchData from './FetchData';
+import Nav from './Nav';
 
 const GetOrders = () => {
-    const {docs} = FetchData('userOrders');
-    console.log(docs)
+    const userOrders = FetchData('userOrders').docs;
+    let docs = FetchData("products").docs;
+    console.log(userOrders);
+    // console.log(docs)
     let { userId } = useParams(); //Fetch params from links
+
+    const or = () => {
+        let dispOrder = userOrders.filter((order)=>{
+            return userOrders["userId"]==userId;
+        })
+        return dispOrder
+    }
+    
     return (
-        <>           
+        <>
+            <Nav
+                typeCategories={[]}
+                brandCategories={[]}
+                navShow={{
+                    typeCat: false,
+                    brandCat: false,
+                    user: false,
+                    cart: false,
+                }}
+                cartItems={[]}
+            />
+
+            <div className="">
+                <div className="container">
+                    <div className="order">
+                        {userOrders && or()}
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
