44a2bfeda547d5d358a1463d9e7bb9ac07803cad
Manan Doshi <manandoshi1301@gmail.com>
2021-05-05 20:48:11 +0530

    Link to order history added

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -225,7 +225,7 @@ const Nav = (props) => {
             <span className={`${styles.architectDaughters}`}>Hello {localStorage.getItem("userName")} !</span></h2>
         </li>
         <li><hr class="dropdown-divider" /></li>
-        <li><a class="dropdown-item fs-5">Order History</a>
+        <li><Link to={`/orderHistory/${localStorage.getItem("userId")}`} class="dropdown-item fs-5">Order History</Link>
         </li>
         <li><a class="dropdown-item fs-5"
           onClick={() => {
