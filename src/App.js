9e633e3e6cc302bcfe166184e28b73b14c6b69c3
Manan Doshi <manandoshi1301@gmail.com>
2021-05-06 14:00:14 +0530

    Route to product Page added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import LogSign from "./components/LogSign/LogSign";
 import Dashboard from "./components/Dashboard/Dashboard";
 import Admin from "./components/LogSign/Admin/Admin";
 import ProductGrid from "./components/ProductGrid/ProductGrid";
+import Product from "./components/ProductGrid/Product";
 import GetOrders from "./components/GetOrders";
 
 function App() {
@@ -21,12 +22,15 @@ function App() {
           <Route exact path="/admin">
             <Admin />
           </Route>
+          <Route path="/product/:categoryType/:itemType/:id">
+            <Product cartItems={cartItems} setCartItems={setCartItems} />
+          </Route>
           <Route path="/product/:categoryType/:itemType">
             <ProductGrid cartItems={cartItems} setCartItems={setCartItems} />
           </Route>
           <Route path="/orderHistory/:userId">
             <GetOrders/>
-          </Route>
+          </Route>          
         </Switch>
       </div>
     </Router>
